9cc44d652d9d9ff80f188364038942311cb6226c
6e6f61 <109554668+6e6f61@users.noreply.github.com>
2023-04-08 22:40:21 +0930

    fix paths

--- flake.nix
+++ flake.nix
@@ -1,9 +1,8 @@
 {
   outputs = { self }:
-  # let template = x: { ${x} = { path = ./. + ${x}; }; };
   let mkTemplates = x: with builtins;
     listToAttrs (map (p:
-      { name = head p; value = { path = ./. + head p; description = elemAt p 1; }; }
+      { name = head p; value = { path = ./. + "/${head p}"; description = elemAt p 1; }; }
     ) x);
   in
     {
